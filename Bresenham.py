85b5c0703ba5229311dc24d27771ea8ea2e49ee9
LorranWassally <88065100+LorranWassally@users.noreply.github.com>
2024-03-26 20:05:38 -0300

    Add files via upload

--- Bresenham.py
+++ Bresenham.py
@@ -2,7 +2,7 @@ def BresIncli(x1,y1,x2,y2):
     incli = (y2-y1)/(x2-x1)
     return incli
 
-def BresFinal(x1,y1,x2,y2):
+def Bres1oct(x1,y1,x2,y2):
     x=x1
     y=y1
     incli = BresIncli(x1,y1,x2,y2)
@@ -16,4 +16,35 @@ def BresFinal(x1,y1,x2,y2):
         e=e+incli
         print(x,y)
     return
-BresFinal(0,0,5,2)
+
+def Bres2oct(x1,y1,x2,y2):
+    x=y1
+    y=x1
+    incli = BresIncli(y1,x1,y2,x2)
+    e=incli+0.5
+    print(y,x)
+    while x < y2:
+        if e >= 1:
+            e=e-1
+            y=y+1
+        x=x+1
+        e=e+incli
+        print(y,x)
+    return
+
+def Bres8oct(x1,y1,x2,y2):
+    x=x1
+    y=y1*-1
+    incli = BresIncli(x1,y1*-1,x2,y2*-1)
+    e=incli+0.5
+    print(x,y*-1)
+    while x < x2:
+        if e >= 1:
+            e=e-1
+            y=y+1
+        x=x+1
+        e=e+incli
+        print(x,y*-1)
+    return
+
+Bres8oct(0,0,5,-2)

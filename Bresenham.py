0701a6ee8c6dc0ec752c6608eb633e7daab8f481
LorranWassally <88065100+LorranWassally@users.noreply.github.com>
2024-04-02 18:58:43 -0300

    Add files via upload

--- Bresenham.py
+++ Bresenham.py
@@ -32,6 +32,81 @@ def Bres2oct(x1,y1,x2,y2):
         print(y,x)
     return
 
+def Bres3oct(x1,y1,x2,y2):
+    x=y1
+    y=x1*-1
+    incli = BresIncli(y1,x1*-1,y2,x2*-1)
+    e=incli+0.5
+    print(y,x*-1)
+    while x < y2:
+        if e >= 1:
+            e=e-1
+            y=y+1
+        x=x+1
+        e=e+incli
+        print(y,x*-1)
+    return
+
+def Bres4oct(x1,y1,x2,y2):
+    x=x1*-1
+    y=y1
+    incli = BresIncli(x1*-1,y1,x2*-1,y2)
+    e=incli+0.5
+    print(x*-1,y)
+    while x < x2*-1:
+        if e >= 1:
+            e=e-1
+            y=y+1
+        x=x+1
+        e=e+incli
+        print(x*-1,y)
+    return
+
+def Bres5oct(x1,y1,x2,y2):
+    x=x1*-1
+    y=y1*-1
+    incli = BresIncli(x1,y1,x2,y2)
+    e=incli+0.5
+    print(x*-1,y*-1)
+    while x < x2*-1:
+        if e >= 1:
+            e=e-1
+            y=y+1
+        x=x+1
+        e=e+incli
+        print(x*-1,y*-1)
+    return
+
+def Bres6oct(x1,y1,x2,y2):
+    x=y1*-1
+    y=x1*-1
+    incli = BresIncli(y1,x1,y2,x2)
+    e=incli+0.5
+    print(y*-1,x*-1)
+    while x < y2*-1:
+        if e >= 1:
+            e=e-1
+            y=y+1
+        x=x+1
+        e=e+incli
+        print(y*-1,x*-1)
+    return
+
+def Bres7oct(x1,y1,x2,y2):
+    x=y1*-1
+    y=x1
+    incli = BresIncli(y1*-1,x1,y2*-1,x2)
+    e=incli+0.5
+    print(y,x*-1)
+    while x < y2*-1:
+        if e >= 1:
+            e=e-1
+            y=y+1
+        x=x+1
+        e=e+incli
+        print(y,x*-1)
+    return
+
 def Bres8oct(x1,y1,x2,y2):
     x=x1
     y=y1*-1
@@ -47,4 +122,4 @@ def Bres8oct(x1,y1,x2,y2):
         print(x,y*-1)
     return
 
-Bres8oct(0,0,5,-2)
+Bres4oct(0,2,-4,6)

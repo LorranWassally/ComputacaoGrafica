c04b6284484d1a78dc18903c09a030aadc955923
LorranWassally <88065100+LorranWassally@users.noreply.github.com>
2024-04-09 19:24:18 -0300

    Add files via upload

---
+++ Transformacoes2.py
@@ -0,0 +1,20 @@
+def translacao(x,y,transx,transy):
+    return (x+transx,y+transy)
+
+def escala(x,y,escx,escy):
+    x1 = round(x*escx)
+    y1 = round(y*escy)
+    return(x1,y1)
+
+def rotacao(x,y,rot):
+    if rot == 30:
+        sen = 0.5
+        cos = 0.85
+    elif rot == 45:
+        sen = 0.65
+        cos = 0.65
+    elif rot == 60:
+        sen = 0.85
+        cos = 0.5
+    
+    return (x*cos+y*-sen,x*sen+y*cos)

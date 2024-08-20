a9eff2c659ad566d93b4541905dca27bf1bc7094
LorranWassally <88065100+LorranWassally@users.noreply.github.com>
2024-08-20 19:54:58 -0300

    Add files via upload

--- GerarCirculo.py
+++ GerarCirculo.py
@@ -1,6 +1,18 @@
+import matplotlib.pyplot as plt
+
+# Lista para armazenar os pontos
+points = []
+
 def Draw8(x, y, xc, yc):
-    print(x,y)
-    pass
+    # Adiciona os pontos à lista
+    points.append((xc + x, yc + y))
+    points.append((xc - x, yc + y))
+    points.append((xc + x, yc - y))
+    points.append((xc - x, yc - y))
+    points.append((xc + y, yc + x))
+    points.append((xc - y, yc + x))
+    points.append((xc + y, yc - x))
+    points.append((xc - y, yc - x))
 
 def AlgCirculo(raio):
     x = raio
@@ -16,14 +28,20 @@ def AlgCirculo(raio):
         else:
             x -= 1
             p += 2 * y - 2 * x + 1
-        
+
         Draw8(x, y, xc=0, yc=0)
-        Draw8(y, x, xc=0, yc=0)
-        Draw8(-x, y, xc=0,yc=0)
-        Draw8(y, -x, xc=0,yc=0)
-        Draw8(x, -y, xc=0,yc=0)
-        Draw8(-y, x, xc=0,yc=0)
-        Draw8(-x, -y, xc=0,yc=0)
-        Draw8(-y, -x, xc=0,yc=0)
 
 AlgCirculo(raio=10)
+
+# Separando os pontos em listas para plotar
+x_points, y_points = zip(*points)
+
+# Criando o gráfico
+plt.figure(figsize=(8, 8))
+plt.scatter(x_points, y_points, c='blue', s=10)
+plt.title('Pontos do Círculo usando o Algoritmo de Bresenham')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.gca().set_aspect('equal', adjustable='box')
+plt.grid(True)
+plt.show()
